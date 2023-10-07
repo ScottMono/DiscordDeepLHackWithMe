264bf4b495c3a79b34acfe010ce91cb33355ef1b
Scott <scott.tippett@deepl.com>
2023-10-07 13:11:04 +0200

    Added version one

---
+++ VersionOne/hackwithme.py
@@ -0,0 +1,59 @@
+# import deepl
+import deepl
+
+import discord
+from discord.ext import commands
+
+# importing os and dotenv to load up the deepl auth key I have stored in my .env file
+import os
+from dotenv import load_dotenv
+
+# import logging
+import logging
+
+# use the load_dotenv function to load the .env file
+load_dotenv()
+
+# populate AUTH_KEY and BOT_TOKEN with the values from our .env file
+AUTH_KEY = os.getenv("AUTH_KEY")
+BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+
+# create a translator instance
+translator = deepl.Translator(AUTH_KEY)
+# event for when the bot is ready
+
+# This example requires the 'message_content' intent.
+
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        print("in here")
+        translated = translate_message("hello", "DE")
+        await message.channel.send(translated)
+
+
+# translate a message
+
+
+def translate_message(message, target_language):
+    return translator.translate_text(message, target_lang=target_language)
+
+
+# assign a logger handler and then run our discord bot client
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+
+client.run(token=BOT_TOKEN, log_handler=handler, log_level=logging.DEBUG)
